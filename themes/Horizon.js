ebc38b4e45ddcc491e2085c68b96659194c6e14e
Adam Tucker <adam.j.tucker@gmail.com>
2020-02-08 20:08:51 -0500

    Add Horizon theme.

---
+++ themes/Horizon.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#131419", "#e95478", "#14d386", "#fab795", "#30aad7", "#b877db", "#1fdad9", "#c7c7c7", "#676767", "#e06783", "#0af29d", "#fac39a", "#56c2ea", "#c38be1", "#3ce8e6", "#feffff"]);
+t.prefs_.set('foreground-color', "#bababa");
+t.prefs_.set('background-color', "#1c1e26");
+t.prefs_.set('cursor-color', 'rgba(199,199,199,0.5)');
